d191455baa437de53f36526fc3d4a104e2fc1c1f
Mohamed Khassar <mohamedkhassar775@gmail.com>
2024-07-15 16:53:08 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar"
+import Slogan from "./components/Slogan"
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Slogan />
+    </>
+  )
+}
+
+export default App
